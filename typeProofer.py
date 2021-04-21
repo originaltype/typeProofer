4616015c9435a5a566d963ec398d732b2a6f526a
Roberto Arista <hello@robertoarista.it>
2021-04-21 11:17:38 +0200

    code review over zoom

--- typeProofer.py
+++ typeProofer.py
@@ -6,27 +6,31 @@ If there is a file calles 'paragraph.txt' in the directory './txt', the script w
 
 # --- Modules ------------------------------------------------------------- #
 import datetime
-now = datetime.datetime.now()
 import os
 from os.path import join, isfile
 from os import listdir
 from pathlib import Path
-
+from drawBot import fill, stroke, font, text, height, newPage, textBox
+from drawBot import installFont, pages, pageCount, saveImage
+from fontTools.ttLib.ttFont import TTFont
 
 # --- Constants ------------------------------------------------------------- #
 
+# just now
+NOW = datetime.datetime.now()
+
 # set your name in the footer
-myName = 'Original Type'
+MY_NAME = 'Original Type'
 
 # set paths to directories with fonts and txt files
-fontFolder = './fonts'
-txtFolder = './txt'
+FONT_FOLDER = './fonts'
+TXT_FOLDER = './txt'
 proofFolder = './proofs'
 
 # set font sizes
-fontSizeLarge = 28
-fontSizeParagraph9 = 9
-fontSizeParagraph12 = 12
+FONT_SIZE_LARGE = 28
+FONT_SIZE_PAR_SMALL = 9
+FONT_SIZE_PAR_MED = 12
 
 # set page dimensions and positions for header and footer items
 PAGE_FORMAT = 'A4Landscape'
@@ -40,14 +44,14 @@ MARGIN_X4 = 200*FROM_MM_TO_PT
 MARGIN_X5 = 270*FROM_MM_TO_PT
 
 # Open the PDF after saving in Preview True or False
-autoOpen = True
+AUTO_OPEN = True
 
 # large sample textbox dimensions
-large = MARGIN_X1, 20*FROM_MM_TO_PT, 262*FROM_MM_TO_PT, 165*FROM_MM_TO_PT
+LARGE = MARGIN_X1, 20*FROM_MM_TO_PT, 262*FROM_MM_TO_PT, 165*FROM_MM_TO_PT
 
 # paragraph sample textbox dimensions
-ParagraphBox1 = MARGIN_X1, 20*FROM_MM_TO_PT, 121*FROM_MM_TO_PT, 162*FROM_MM_TO_PT
-ParagraphBox2 = MARGIN_X1*8, 20*FROM_MM_TO_PT, 121*FROM_MM_TO_PT, 162*FROM_MM_TO_PT
+BOX1 = MARGIN_X1, 20*FROM_MM_TO_PT, 121*FROM_MM_TO_PT, 162*FROM_MM_TO_PT
+BOX2 = MARGIN_X1*8, 20*FROM_MM_TO_PT, 121*FROM_MM_TO_PT, 162*FROM_MM_TO_PT
 
 
 # --- Functions ------------------------------------------------------------- #
@@ -58,17 +62,17 @@ def typeAttributes():
     font('SFMono-Regular', 8)
 
 # draw the header and footer
-def drawHeaderFooter():
+def drawHeaderFooter(postscriptFontName, fileName):
     p = Path(fileName)
-    projectName = fontName.split('-')
+    familyName = postscriptFontName.split('-')[0]
     typeAttributes()
-    text('Project:' + ' ' + projectName[0], (MARGIN_X1, height()-14*FROM_MM_TO_PT), align='left')
-    text(f'Date: {now:%Y-%m-%d %H:%M}', (MARGIN_X2, height()-14*FROM_MM_TO_PT), align='left')
-    text(f'Fontfile: {fontName}', (MARGIN_X3, height()-14*FROM_MM_TO_PT))
-    text('Characterset:' + ' ' + p.stem, (MARGIN_X4, height()-14*FROM_MM_TO_PT))
+    text(f'Project: {familyName}', (MARGIN_X1, height()-14*FROM_MM_TO_PT), align='left')
+    text(f'Date: {NOW:%Y-%m-%d %H:%M}', (MARGIN_X2, height()-14*FROM_MM_TO_PT), align='left')
+    text(f'Fontfile: {postscriptFontName}', (MARGIN_X3, height()-14*FROM_MM_TO_PT))
+    text(f'Characterset: {p.stem}', (MARGIN_X4, height()-14*FROM_MM_TO_PT))
     text(f'Page {pageCount()-1:0>2d}/', (MARGIN_X5, height()-14*FROM_MM_TO_PT), align='right')
-    text(f'© {now:%Y}' + ' ' + myName, (MARGIN_X1, MARGIN_BOTTOM/2))
-    
+    text(f'© {NOW:%Y}' + ' ' + MY_NAME, (MARGIN_X1, MARGIN_BOTTOM/2))
+
 # read lines from a texfile
 def readStringsFromFile(fileName):
     with open(fileName, mode='r', encoding='utf-8') as txtFile:
@@ -84,58 +88,67 @@ def collectFilesPaths(folder, extension=''):
             paths.append(eachPath)
     return paths
 
-# draw a page
-def initPage():
-    t1 = '\n'.join(proofSet)
-    t2 = '\n'.join(proofSet)
-    while len(t1):
+def sortFonts(fontPaths):
+    return sorted(fontPaths, key=lambda ff: TTFont(file=ff)["OS/2"].usWeightClass)
+
+def drawTwoColumnsLayout(txt, postscriptFontName):
+    typeAttributes()
+    text(f'{postscriptFontName} 9 pts', (MARGIN_X1, height()-25*FROM_MM_TO_PT))
+    font(postscriptFontName, FONT_SIZE_PAR_SMALL)
+    textBox(f'{txt}', (BOX1))
+    typeAttributes()
+    text(f'{postscriptFontName} 12 pts', (MARGIN_X1*8, height()-25*FROM_MM_TO_PT))
+    font(postscriptFontName, FONT_SIZE_PAR_MED)
+    textBox(f'{txt}', (BOX2))
+    # we don't aknowledge overflow, so we return an empty string
+    return ""
+
+def drawOneColumnsLayout(txt, postscriptFontName):
+    font(postscriptFontName, FONT_SIZE_LARGE)
+    txt = textBox(f'{txt}', (LARGE))
+    return txt
+
+def drawProof(proofSet, postscriptFontName):
+    txt = '\n'.join(proofSet)
+    while txt:
         newPage(PAGE_FORMAT)
-        fill(1)
         fill(0)
         if fileName == './txt/paragraph.txt':
-            typeAttributes()
-            text(f'{fontName}'+' '+'9 pts', (MARGIN_X1, height()-25*FROM_MM_TO_PT))
-            font(fontName, fontSizeParagraph9)
-            t1 = textBox(f'{t1}', (ParagraphBox1))
-            typeAttributes()
-            text(f'{fontName}'+' '+'12 pts', (MARGIN_X1*8, height()-25*FROM_MM_TO_PT))
-            font(fontName, fontSizeParagraph12)
-            t2 = textBox(f'{t2}', (ParagraphBox2))
+            txt = drawTwoColumnsLayout(txt, postscriptFontName)
         else:
-            font(fontName, fontSizeLarge)
-            t1 = textBox(f'{t1}', (large))
-        drawHeaderFooter()
-        
+            txt = drawOneColumnsLayout(txt, postscriptFontName)
+        drawHeaderFooter(postscriptFontName, fileName)
+
 
 # --- Instructions ------------------------------------------------------------- #
+if __name__ == '__main__':
+    # store fonts and txt files in a variable
+    allFonts = sortFonts(collectFilesPaths(FONT_FOLDER))
+    alltxtFiles = collectFilesPaths(TXT_FOLDER)
 
-# store fonts and txt files in a variable
-allFonts = collectFilesPaths(fontFolder)
-alltxtFiles = collectFilesPaths(txtFolder)
+    # iterate over all fonts and text files and draw the pages
+    for eachFontPath in allFonts:
+        for fileName in alltxtFiles:
+            postscriptFontName = installFont(eachFontPath)
+            proofSet = readStringsFromFile(fileName)
+            drawProof(proofSet, postscriptFontName)
 
-# iterate over all fonts and text files and draw the pages
-for eachFontPath in allFonts:
-    for fileName in alltxtFiles:
-        fontName = installFont(eachFontPath)
-        proofSet = readStringsFromFile(fileName)
-        initPage()
-    
-# get all pages
-allPages = pages()
-# count how many pages are available
-totalPages = len(allPages)
-# loop over allpages
-for page in allPages:
-    # set the page as current context
-    with page:
-        # draw the total pagecount in each of them
-        typeAttributes()
-        text(f'{totalPages}', (270*FROM_MM_TO_PT, height()-14*FROM_MM_TO_PT), align='left')
+    # get all pages
+    allPages = pages()
+    # count how many pages are available
+    totalPages = len(allPages)
+    # loop over allpages
+    for page in allPages:
+        # set the page as current context
+        with page:
+            # draw the total pagecount in each of them
+            typeAttributes()
+            text(f'{totalPages}', (270*FROM_MM_TO_PT, height()-14*FROM_MM_TO_PT), align='left')
 
-projectName = fontName.split('-')
+    projectName = postscriptFontName.split('-')
 
-saveTo=f'{proofFolder}/{projectName[0]}-proof-{now:%Y-%m-%d}.pdf'
-saveImage(saveTo)
+    saveTo = f'{proofFolder}/{projectName[0]}-proof-{NOW:%Y-%m-%d}.pdf'
+    saveImage(saveTo)
 
-if autoOpen:
-    os.system(f"open -a Preview {saveTo}")
+    if AUTO_OPEN:
+        os.system(f"open -a Preview {saveTo}")
